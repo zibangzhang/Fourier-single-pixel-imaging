8ab16e51e103490c08eae8f925b2f4ddd0564911
Zibang Zhang <41044700+zibangzhang@users.noreply.github.com>
2022-09-29 10:55:32 +0800

    Delete getOrderMatFilename.m

--- getOrderMatFilename.m
+++
@@ -1,6 +0,0 @@
-function [ orderMatFilename ] = getOrderMatFilename( mRow, nCol, PathStr )
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
-    orderMatFilename = sprintf('%sOrderMat_%d_%d.mat', PathStr, mRow, nCol);
-end
-
